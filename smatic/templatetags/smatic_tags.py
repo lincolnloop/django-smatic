93111efd82b58e6843e10bb2c3a0e08a543644d9
Marco Louro <mlouro@gmail.com>
2011-03-02 16:18:59 +0000

    changed ROOT to PATH

--- smatic/templatetags/smatic_tags.py
+++ smatic/templatetags/smatic_tags.py
@@ -10,7 +10,7 @@ def scss(file_path):
     """
         Converts an scss file into css and returns the output
     """
-    input_path = safe_join(settings.SMATIC_SCSS_ROOT, file_path)
+    input_path = safe_join(settings.SMATIC_SCSS_PATH, file_path)
     if not os.path.exists(input_path):
         raise Exception('File does not exist: %s\n' % input_path)
 
@@ -27,7 +27,7 @@ register.simple_tag(scss)
 
 def js(file_path):
 
-    input_path = safe_join(settings.SMATIC_JS_ROOT, file_path)
+    input_path = safe_join(settings.SMATIC_JS_PATH, file_path)
     if not os.path.exists(input_path):
         # TODO: check if enabled on
         raise Exception('File does not exist: %s\n' % input_path)
