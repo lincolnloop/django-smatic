1a16d598c902218a8112841219f89044724155da
Chris Beaven <smileychris@gmail.com>
2011-03-03 16:42:42 +1300

    Tidy up the code, and don't make settings.SASS_BIN a requirement (default to 'sass')

--- smatic/templatetags/smatic_tags.py
+++ smatic/templatetags/smatic_tags.py
@@ -6,32 +6,35 @@ from django.utils._os import safe_join
 
 register = template.Library()
 
+
+@register.simple_tag
 def scss(file_path):
     """
-        Converts an scss file into css and returns the output
+    Convert an scss file into css and returns the output.
     """
     input_path = safe_join(settings.SMATIC_SCSS_PATH, file_path)
     if not os.path.exists(input_path):
         raise Exception('File does not exist: %s\n' % input_path)
 
-    sass_dict = { 'bin' : settings.SASS_BIN, 'sass_style' : 'compact', 'input' : input_path }
-    cmd = "%(bin)s --scss -t %(sass_style)s -C %(input)s" % sass_dict
+    cmd = "%(bin)s --scss -t %(sass_style)s -C %(input)s" % {
+        'bin': getattr(settings, 'SASS_BIN', 'sass'),
+        'sass_style': 'compact',
+        'input': input_path,
+    }
     (status, output) = getstatusoutput(cmd)
     if not status == 0:
         raise Exception(output)
 
     return output
 
-register.simple_tag(scss)
 
-
+@register.simple_tag
 def js(file_path):
-
     input_path = safe_join(settings.SMATIC_JS_PATH, file_path)
     if not os.path.exists(input_path):
         # TODO: check if enabled on
         raise Exception('File does not exist: %s\n' % input_path)
 
-    return '<script type="text/javascript" src="%sjs/%s"></script>' % (settings.STATIC_URL, file_path)
-
-register.simple_tag(js)
+    return '<script type="text/javascript" src="%sjs/%s"></script>' % (
+        settings.STATIC_URL, file_path
+    )
